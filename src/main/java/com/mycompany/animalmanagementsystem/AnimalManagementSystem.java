2e6cd2d5d842f8272f393d8ef97aaf4125f9ef24
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2025-01-16 11:01:12 +0000

    First commit test

---
+++ src/main/java/com/mycompany/animalmanagementsystem/AnimalManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.animalmanagementsystem;
+
+/**
+ *
+ * @author francescosciabbarrasi
+ */
+public class AnimalManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("First line:" + "Hello World!");
+    }
+}
