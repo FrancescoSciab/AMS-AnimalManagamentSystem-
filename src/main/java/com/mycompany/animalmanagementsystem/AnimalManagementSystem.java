f074c9c732b7fd3a08748e18e9d1aae07db9b380
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2025-01-16 16:18:54 +0000

    Successfully reading example file I created to test application throughout its development process

--- src/main/java/com/mycompany/animalmanagementsystem/AnimalManagementSystem.java
+++ src/main/java/com/mycompany/animalmanagementsystem/AnimalManagementSystem.java
@@ -3,14 +3,25 @@
  */
 
 package com.mycompany.animalmanagementsystem;
-
-/**
- *
- * @author francescosciabbarrasi
- */
+import java.io.File;
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
+//import java.io.IOException;
 public class AnimalManagementSystem {
 
     public static void main(String[] args) {
-        System.out.println("First line:" + "Hello World!");
+        try {
+        File fileEntered = new File("animals.txt");
+        Scanner myReader = new Scanner(fileEntered);
+        
+        while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+          myReader.close();
+        } catch (FileNotFoundException e) {
+          System.out.println("An error occurred reading your file");
+          e.printStackTrace();
+        }
     }
 }
