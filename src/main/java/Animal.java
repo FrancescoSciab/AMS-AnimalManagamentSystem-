b37fc3091c597d8125c6c4eb8019534767242680
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2025-01-19 12:51:06 +0000

    Adding subclasses: Mammal, bird, reptile, fish

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -40,3 +40,64 @@ abstract class Animal {
         return species;
     }
 }
+
+class Mammal extends Animal {
+    private String furType;
+
+    public Mammal(String species, String name, String habitat, String dob, double weight, String furType) {
+        super(species, name, habitat, dob, weight);
+        this.furType = furType;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Mammal [Species=" + species + ", Name=" + name + ", Habitat=" + habitat + 
+                ", DoB=" + dob + ", Weight=" + weight + "kg, Fur Type=" + furType + "]");
+    }
+}
+
+class Bird extends Animal {
+    private double wingSpan;
+
+    public Bird(String species, String name, String habitat, String dob, double weight, double wingSpan) {
+        super(species, name, habitat, dob, weight);
+        this.wingSpan = wingSpan;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Bird [Species=" + species + ", Name=" + name + ", Habitat=" + habitat + 
+                ", DoB=" + dob + ", Weight=" + weight + "kg, Wing Span=" + wingSpan + "cm]");
+    }
+}
+
+class Reptile extends Animal {
+    private boolean isVenomous;
+
+    public Reptile(String species, String name, String habitat, String dob, double weight, boolean isVenomous) {
+        super(species, name, habitat, dob, weight);
+        this.isVenomous = isVenomous;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Reptile [Species=" + species + ", Name=" + name + ", Habitat=" + habitat + 
+                ", DoB=" + dob + ", Weight=" + weight + "kg, Venomous=" + isVenomous + "]");
+    }
+}
+
+class Fish extends Animal {
+    private String waterType;
+
+    public Fish(String species, String name, String habitat, String dob, double weight, String waterType) {
+        super(species, name, habitat, dob, weight);
+        this.waterType = waterType;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Fish [Species=" + species + ", Name=" + name + ", Habitat=" + habitat + 
+                ", DoB=" + dob + ", Weight=" + weight + "kg, Water Type=" + waterType + "]");
+    }
+}
+
